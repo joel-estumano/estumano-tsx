aeacc3096f30aa06264434c6569b8ecd331edb90
Joel Estumano <joelestumano@gmail.com>
2022-11-14 18:38:08 -0300

    first commit (Fix: html tags)

--- src/index/components/navbar/Navbar.tsx
+++ src/index/components/navbar/Navbar.tsx
@@ -13,29 +13,30 @@ export function Navbar() {
         <div className="nav-component fixed-top">
             <div className="nav-component-bar">
                 <nav className="nav-ref navbar navbar-expand-lg sticky-top">
-                    <div className="container-ref container space-between text-uppercase"></div>
-                    <a className="navbar-brand d-flex justify-content-center align-items-center text-white">
-                        <img className="me-1" src={reactLogo} alt="Logo" width="32" height="32" />
-                        <span>Estumano</span>
-                    </a>
-                    <button className="navbar-toggler border-0 text-white shadow-none" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="material-symbols-outlined">menu</span>
-                    </button>
-                    <div className="d-inline-flex w-100">
-                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                            <ul className="navbar-nav me-auto mb-0 mb-lg-0 w-100 justify-content-end">
-                                <li className="nav-item">
+                    <div className="container-ref container space-between text-uppercase">
+                        <a className="navbar-brand d-flex justify-content-center align-items-center text-white">
+                            <img className="me-1" src={reactLogo} alt="Logo" width="32" height="32" />
+                            <span>Estumano</span>
+                        </a>
+                        <button className="navbar-toggler border-0 text-white shadow-none" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <span className="material-symbols-outlined">menu</span>
+                        </button>
+                        <div className="d-inline-flex w-100">
+                            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                                <ul className="navbar-nav me-auto mb-0 mb-lg-0 w-100 justify-content-end">
                                     {
                                         React.Children.toArray(
                                             routerLinks.map((routerLink) =>
-                                                <a className="nav-link d-flex justify-content-start align-items-center text-decoration-none text-white" style={{ padding: '8px 16px' }}>
-                                                    <span className="text-uppercase" style={{ fontSize: '12px', cursor: 'pointer' }}> {routerLink} </span>
-                                                </a>
+                                                <li className="nav-item">
+                                                    <a className="nav-link d-flex justify-content-start align-items-center text-decoration-none text-white" style={{ padding: '8px 16px' }}>
+                                                        <span className="text-uppercase" style={{ fontSize: '12px', cursor: 'pointer' }}> {routerLink} </span>
+                                                    </a>
+                                                </li>
                                             )
                                         )
                                     }
-                                </li>
-                            </ul>
+                                </ul>
+                            </div>
                         </div>
                     </div>
                 </nav>
