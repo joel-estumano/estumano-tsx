f8ef15baa2cefd58164608ca65a06e7f311669c9
Joel Estumano <joelestumano@gmail.com>
2022-11-14 18:19:03 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Navbar } from './index/components/navbar/Navbar'
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar></Navbar>
+    </div> 
+  )
+}
+
+export default App
