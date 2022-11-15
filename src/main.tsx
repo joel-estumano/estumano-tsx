a53d92658baa43948e752afbad8a19e026eb121e
Joel Estumano <joelestumano@gmail.com>
2022-11-15 12:39:13 -0300

    first commit (Fix: fonts)

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-//
+//styles
 import './index.scss'
+import "./assets/fonts/poppins.scss";
+//scripts
 import "bootstrap/dist/js/bootstrap.bundle.min"
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
